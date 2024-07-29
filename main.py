b497326a162c5ba5249ea6f3f5ae28c2521d4c5c
Urban <elenapolevschikova75@gmail.com>
2024-07-29 13:55:53 +0300

    Добавление функций

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
-from fastapi import FastAPI    # Установили фреймворк FastAPI при помощи пакетного менеджера pip.
+# -*- coding: utf-8 -*-
+from fastapi import FastAPI, Path    # Установили фреймворк FastAPI при помощи пакетного менеджера pip.
+from typing import Annotated
 
 
-app = FastAPI()    # Создаkb приложение(объект) FastAPI предварительно импортировав класс для него.
+app = FastAPI()    # Создали приложение(объект) FastAPI предварительно импортировав класс для него.
+
+
+@app.get("/")  # если мы получили .get("/")-гет запрос
+async def welcome() -> dict:  # то отработай эту функцию
+    return {"message": "Hello world!"}
 
 
 @app.get("/")    # Создайли маршрут к главной странице - "/".
@@ -25,6 +32,19 @@ async def Get_User_Info(username: str, age: int) -> dict:
     return {"message": f"Информация о пользователе {username}", "Age": age}
     # По нему должно выводиться сообщение "Информация о пользователе. Имя: <username>, Возраст: <age>".
 
+
+@app.get("/user/{user-id}")  # если мы получили .get("/")-гет запрос
+async def Get_Main_Page(user_id: Annotated[int, Path(ge=1, le=100, description="Enter User ID", example="1")]) -> dict:
+    # Path - провер.какой тип данных приходит и хранит их, Annotated - помогает работать с большестроками
+    return {"message": f"Hello, {user_id}"}
+
+
+@app.get("/user/{username}/{age}")  # если мы получили .get("/")-гет запрос
+async def Get_Main_Page(username: Annotated[str, Path(min_length=5, max_length=20, description="Enter username",
+                        example="UrbanUser")], age: int = Path(ge=18, le=120,
+                        description="Enter age", example="24")) -> dict:
+    # Path - проверяет какой тип данных приходит и хранит их, Annotated - помогает работать с большестроками
+    return {"message": f"Hello, {username} {age}"}
 
 
 # @app.get("/id")
