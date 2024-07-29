61c822d187dd545caea54c574b48b86c78e01bbf
Urban <elenapolevschikova75@gmail.com>
2024-07-29 14:41:47 +0300

    Функции 5

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from fastapi import FastAPI    # Установили фреймворк FastAPI при помощи пакетного менеджера pip.
+from fastapi import FastAPI, Path   # Установили фреймворк FastAPI при помощи пакетного менеджера pip.
+from typing import Annotated
 
-
-app = FastAPI()    # Создаkb приложение(объект) FastAPI предварительно импортировав класс для него.
+app = FastAPI()    # Создали приложение(объект) FastAPI предварительно импортировав класс для него.
 
 
 @app.get("/")    # Создайли маршрут к главной странице - "/".
@@ -9,7 +9,7 @@ async def Get_Main_Page() -> dict:
     return {"message": "Главная страница"}   # По нему должно выводиться сообщение "Главная страница".
 
 
-@app.get("/user/admin}")    # Создайте маршрут к странице администратора - "/user/admin".
+@app.get("/user/admin}")    # Создали маршрут к странице администратора - "/user/admin".
 async def Get_Admin_Page() -> dict:
     return {"message": f"Вы вошли как администратор!"}  # По нему должно вывод-ся сообщение "Вы вошли как администратор"
 
@@ -20,11 +20,24 @@ async def Get_User_Number(user_id: int) -> dict:
     # По нему должно выводиться сообщение "Вы вошли как пользователь № <user_id>".
 
 
-@app.get("/user")   # Создайте маршрут к страницам пользователей передавая данные в адресной строке - "/user".
+@app.get("/user")   # Создали маршрут к страницам пользователей передавая данные в адресной строке - "/user".
 async def Get_User_Info(username: str, age: int) -> dict:
     return {"message": f"Информация о пользователе {username}", "Age": age}
     # По нему должно выводиться сообщение "Информация о пользователе. Имя: <username>, Возраст: <age>".
 
+
+@app.get("/user/{user_id}")  # если мы получили .get("/")-гет запрос
+async def Get_Main_Page(user_id: Annotated[int, Path(ge=5, le=20, description="Enter User ID", example="1")]) -> dict:
+    # Path - проверяет какой тип данных приходит,и хранит их
+    return {"message": f"Hello, {user_id}"}
+
+
+@app.get("/user/{username}/{age}")  # если мы получили .get("/")-гет запрос
+async def Get_Main_Page(username: Annotated[str, Path(min_length=5, max_length=20
+    , description="Enter username", example="UrbanUser")]
+               , age: int = Path(ge=18, le=120, description="Enter age", example="24")) -> dict:
+    # Path - проверяет какой тип данных приходит и хранит их, Annotated - помогает работать с большестроками
+    return {"message": f"Hello, {username} {age}"}
 
 
 # @app.get("/id")
